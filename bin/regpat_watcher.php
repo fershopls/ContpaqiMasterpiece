506d59374caf368e4da7bd7313a046fdaa1e0132
fershopls <shaaf4ever@gmail.com>
2017-10-25 18:05:32 -0500

    Regpat watcher fix

--- bin/regpat_watcher.php
+++ bin/regpat_watcher.php
@@ -2,14 +2,34 @@
 /* @var \lib\Bin\App $app*/
 $app = isset($app)?$app:require_once(realpath(__DIR__) . '/../bootstrap.php');
 
-use lib\Data\RequestsManager;
-$requestHandler = new RequestsManager();
-$config = $requestHandler->get(['update'=>true,'time'=>time()])->on($settings->get('DIRS.request'));
+$request_file = $settings->get('DIRS.request').DIRECTORY_SEPARATOR.$settings->get('APPS.LIR.request_regpat_file');
+$default = array('time'=>time());
 
-if ($config)
+if (file_exists($request_file))
 {
-    echo PHP_EOL.'[REGPATS] Updating regpats by user request on '.date('His-mdY', $config['time']).'...';
+    // Open
+    $req = json_decode(file_get_contents($request_file), true);
+    $req = array_merge([], $default, $req);
+    if (isset($req['opened']))
+    {
+        // Alredy opened
+        echo PHP_EOL."LIR regpat request json file already working with. This could mean crash.";
+        $req = false;
+    } else {
+        $req['opened'] = time();
+        file_put_contents($request_file, json_encode($req, JSON_PRETTY_PRINT));
+    }
+} else {
+    $req = false;
+}
+
+// No esta jalando esto
+if ($req)
+{
+    echo PHP_EOL.'[REGPATS] Updating regpats by user request on '.date('His-mdY', $req['time']).'...';
     require_once 'get_regpats.php';
-    $config['time_done'] = time();
-    file_put_contents($settings->get('DIRS.request').DIRECTORY_SEPARATOR.$settings->get('APPS.LIR.request_regpat_file'), json_encode($config, JSON_PRETTY_PRINT));
+    var_dump($req);
+    echo PHP_EOL.'Done.'.PHP_EOL;
+    $req['time_done'] = time();
+    file_put_contents($request_file, json_encode($req, JSON_PRETTY_PRINT));
 }
--- frontend/lir/regpat_refresh.php
+++ frontend/lir/regpat_refresh.php
@@ -1,7 +1,7 @@
 <?php
 // Init Script
 require_once realpath(__DIR__) . '/../../bootstrap.php';
-$request_dir = $settings->get('DIRS.request');
-file_put_contents($request_dir . '/regpat.json', '{"update":true,"time":'.time().'}')
+$request_file = $settings->get('DIRS.request').DIRECTORY_SEPARATOR.$settings->get('APPS.LIR.request_regpat_file');
+file_put_contents($request_file, '{"update":true,"time":'.time().'}')
 ?>
 <script>window.history.back();</script>
